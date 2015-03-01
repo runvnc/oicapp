c36db7fe607a9e77534c702bbfe6360e06826e46
Jason Livesay <ithkuil@gmail.com>
2015-03-01 01:45:43 +0000

    updates

--- webserver.nim
+++ webserver.nim
@@ -1,13 +1,15 @@
 import strutils, sockets, httpserver, threadpool, os
 
-var secret = 'JIY234lksOIJOkX923sA';
+var secret = "JIY234lksOIJOkX923sA";
 
 proc handleRequest(client: Socket, path, query: string): bool {.procvar.} =
+  echo(path);
   var parts = split(path, '/')
-  if parts[0] == secret:
+  if parts[1] == secret:
+    echo("request ok");
     client.send("ok")
     client.close
-    discard spawn execShellCmd("./createserver " & join(parts, " "))
+    discard spawn execShellCmd("./createserver " & join(parts[2..4], " "))
 
-run(handleRequest, Port(5000))
+run(handleRequest, Port(6001))
 
